5641cc3e98245aeac8d5b982315b34a60ddb2838
Amaan63 <amaansayed63@gmail.com>
2025-05-15 22:22:26 +0530

    created JWT provider

---
+++ backend/src/main/java/com/blogeditor/config/JwtProvider.java
@@ -0,0 +1,35 @@
+package com.blogeditor.config;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.Authentication;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtProvider {
+
+    private static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    public static String generatedToken(Authentication auth) {
+        return Jwts.builder()
+                .setIssuer("Amaan")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 86400000))
+                .claim("email", auth.getName())
+                .signWith(key)
+                .compact();
+    }
+
+    public static String getEmailFromJwtToken(String jwt) {
+        // Bearer token
+        jwt = jwt.substring(7);
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+        String email = String.valueOf(claims.get("email"));
+        return email;
+    }
+}
