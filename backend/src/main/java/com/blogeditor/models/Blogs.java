13c361c72b0c7eb057898e502cff427fcb8553a1
Amaan63 <amaansayed63@gmail.com>
2025-05-15 20:27:28 +0530

    created blog entity

---
+++ backend/src/main/java/com/blogeditor/models/Blogs.java
@@ -0,0 +1,37 @@
+package com.blogeditor.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Blogs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String tags;
+
+    @Enumerated(EnumType.STRING)
+    private Status status; // Using imported enum
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
