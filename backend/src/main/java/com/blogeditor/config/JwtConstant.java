6ed8c50c968321bfe52bc6925ce1433ef93c051a
Amaan63 <amaansayed63@gmail.com>
2025-05-15 22:22:16 +0530

    created JWT constant

---
+++ backend/src/main/java/com/blogeditor/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.blogeditor.config;
+
+public class JwtConstant {
+    public static String JWT_HEADER = "Authorization";
+    public static String SECRET_KEY = "cxsd254SD42FZD5xcscnvdahvxgashzgdzxlkdlz25563znaxhbzxyRRWHEGDQJhgdxbzxmcnxkcjoxxc";
+}
