f1b97ce7fb560b12ddc0517f4befd7afa488ff94
Amaan63 <amaansayed63@gmail.com>
2025-05-15 20:35:18 +0530

    Created Blog Repository

---
+++ backend/src/main/java/com/blogeditor/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.blogeditor.repository;
+
+import com.blogeditor.models.Blogs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blogs,Long> {
+}
