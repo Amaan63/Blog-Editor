119cf72087418daaadca0a12a1ec0175c504c556
Amaan63 <amaansayed63@gmail.com>
2025-05-15 21:41:56 +0530

    added method to find blogs by Id

--- backend/src/main/java/com/blogeditor/repository/BlogRepository.java
+++ backend/src/main/java/com/blogeditor/repository/BlogRepository.java
@@ -4,6 +4,11 @@ import com.blogeditor.models.Blogs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BlogRepository extends JpaRepository<Blogs,Long> {
+
+    public List<Blogs> findByBlogsId(Long blogId);
+
 }
