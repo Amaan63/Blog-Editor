d2138a887446f0562072e0ffe0755760cc941497
Amaan63 <amaansayed63@gmail.com>
2025-05-15 20:27:17 +0530

    Created user entity

---
+++ backend/src/main/java/com/blogeditor/models/User.java
@@ -0,0 +1,34 @@
+package com.blogeditor.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Blogs> posts = new ArrayList<>();
+
+
+}
