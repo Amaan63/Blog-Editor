3f2d6fe97101a0ef4d193487b7d35f4cd1538873
Amaan63 <amaansayed63@gmail.com>
2025-05-15 22:23:09 +0530

    minor changes

--- backend/src/main/java/com/blogeditor/service/Blog/BlogServiceImplementation.java
+++ backend/src/main/java/com/blogeditor/service/Blog/BlogServiceImplementation.java
@@ -6,11 +6,12 @@ import com.blogeditor.models.Status;
 import com.blogeditor.repository.BlogRepository;
 import com.blogeditor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
+@Service
 public class BlogServiceImplementation implements BlogService {
 
     @Autowired
