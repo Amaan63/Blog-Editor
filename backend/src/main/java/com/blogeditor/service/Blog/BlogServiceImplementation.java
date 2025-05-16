4b51a8b0d81355c86b6b63be0bdf4eb89b3ca175
Amaan63 <amaansayed63@gmail.com>
2025-05-16 12:53:01 +0530

    created method for saveorupdate,publish,getall blogs,getblog by id service methods

--- backend/src/main/java/com/blogeditor/service/Blog/BlogServiceImplementation.java
+++ backend/src/main/java/com/blogeditor/service/Blog/BlogServiceImplementation.java
@@ -3,6 +3,7 @@ package com.blogeditor.service.Blog;
 import com.blogeditor.dto.BlogDto;
 import com.blogeditor.models.Blogs;
 import com.blogeditor.models.Status;
+import com.blogeditor.models.User;
 import com.blogeditor.repository.BlogRepository;
 import com.blogeditor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BlogServiceImplementation implements BlogService {
@@ -21,56 +23,65 @@ public class BlogServiceImplementation implements BlogService {
     private UserRepository userRepository; // Assuming you need to set the user
 
     @Override
-    public Blogs saveOrUpdateDraft(BlogDto reqBlog) throws Exception {
+    public Blogs saveOrUpdateDraft(BlogDto reqBlog, User user) throws Exception {
         Blogs blog;
 
-        if (reqBlog.getBlogId() == null) {
-            // Create new blog draft
-            blog = new Blogs();
-            blog.setCreatedAt(LocalDateTime.now());
-        } else {
-            // Update existing blog draft
+        if (reqBlog.getBlogId() != null) {
+            System.out.println("Blog ID: " + reqBlog.getBlogId());
+            // Try to fetch and update existing blog
             blog = blogRepository.findById(reqBlog.getBlogId())
                     .orElseThrow(() -> new Exception("Blog not found with id: " + reqBlog.getBlogId()));
+        } else {
+            // Create new blog
+            blog = new Blogs();
+            blog.setCreatedAt(LocalDateTime.now());
+
+            // Set user on new blog
+            blog.setUser(userRepository.findById(user.getId())
+                    .orElseThrow(() -> new Exception("User not found")));
         }
 
-        // Common fields to set for both create and update
+        // Set/update common fields
         blog.setTitle(reqBlog.getTitle());
         blog.setContent(reqBlog.getContent());
         blog.setTags(reqBlog.getTags());
         blog.setStatus(Status.DRAFT);
         blog.setUpdatedAt(LocalDateTime.now());
 
-        // Set user only if new (or you can allow updating user if needed)
-        // if (blog.getUser() == null) {
-        // User user = userRepository.findById(reqBlog.getUserId())
-        // .orElseThrow(() -> new Exception("User not found"));
-        // blog.setUser(user);
-        // }
-
         return blogRepository.save(blog);
     }
 
-    @Override
-    public Blogs publishBlog(BlogDto reqBlog) throws Exception {
-        Blogs blog = new Blogs();
 
+
+    @Override
+    public Blogs publishBlog(BlogDto reqBlog, User user) throws Exception {
+        Blogs blog;
+
+        if (reqBlog.getBlogId() != null) {
+            // Update existing blog
+            blog = blogRepository.findById(reqBlog.getBlogId())
+                    .orElseThrow(() -> new Exception("Blog not found with id: " + reqBlog.getBlogId()));
+        } else {
+            // Create new blog
+            blog = new Blogs();
+            blog.setCreatedAt(LocalDateTime.now());
+
+            // Set user for new blog
+            User dbUser = userRepository.findById(user.getId())
+                    .orElseThrow(() -> new Exception("User not found"));
+            blog.setUser(dbUser);
+        }
+
+        // Common fields for both create and update
         blog.setTitle(reqBlog.getTitle());
         blog.setContent(reqBlog.getContent());
         blog.setTags(reqBlog.getTags());
         blog.setStatus(Status.PUBLISHED);
-
-        LocalDateTime now = LocalDateTime.now();
-        blog.setCreatedAt(now);
-        blog.setUpdatedAt(now);
-
-        // Set user
-//        User user = userRepository.findById(reqBlog.getUserId())
-//                .orElseThrow(() -> new Exception("User not found"));
-//        blog.setUser(user);
+        blog.setUpdatedAt(LocalDateTime.now());
 
         return blogRepository.save(blog);
     }
+
 
     @Override
     public List<Blogs> getAllBlogs() throws Exception {
@@ -78,10 +89,10 @@ public class BlogServiceImplementation implements BlogService {
     }
 
     @Override
-    public List<Blogs> getBlogsByUserId(Long blogId) throws Exception {
-        List<Blogs> blogs = blogRepository.findByBlogsId(blogId);
+    public Optional<Blogs> getBlogsById(Long blogId) throws Exception {
+        Optional<Blogs> blogs = blogRepository.findById(blogId);
         if (blogs.isEmpty()) {
-            throw new Exception("No blogs found for user ID: " + blogId);
+            throw new Exception("No blogs found for the given ID: " + blogId);
         }
         return blogs;
     }
