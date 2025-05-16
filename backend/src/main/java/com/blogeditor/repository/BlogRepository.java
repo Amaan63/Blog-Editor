c00c2ed9648c43c1f601dddd29709440c7f56f61
Amaan63 <amaansayed63@gmail.com>
2025-05-16 12:51:59 +0530

    removed the method

--- backend/src/main/java/com/blogeditor/repository/BlogRepository.java
+++ backend/src/main/java/com/blogeditor/repository/BlogRepository.java
@@ -4,11 +4,8 @@ import com.blogeditor.models.Blogs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blogs,Long> {
-
-    public List<Blogs> findByBlogsId(Long blogId);
-
 }
