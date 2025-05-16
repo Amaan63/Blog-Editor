aa31a43b8c19a7d19808761af1c813aa3574802b
Amaan63 <amaansayed63@gmail.com>
2025-05-16 20:44:22 +0530

    Search Bar component is created

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,23 @@
+import { Search } from "lucide-react";
+
+const SearchBar = ({ value, onChange, onSearch }) => {
+  return (
+    <div className="flex items-center space-x-2">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Search blog by ID"
+        className="flex-grow px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button
+        onClick={onSearch}
+        className="p-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"
+      >
+        <Search size={20} />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
