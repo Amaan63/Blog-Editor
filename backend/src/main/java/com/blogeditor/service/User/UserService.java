b9f23a3be5b4c20915a1c57e69ac445cda3ed4c3
Amaan63 <amaansayed63@gmail.com>
2025-05-16 11:40:02 +0530

    Changed the parameter to userDto

--- backend/src/main/java/com/blogeditor/service/User/UserService.java
+++ backend/src/main/java/com/blogeditor/service/User/UserService.java
@@ -1,11 +1,12 @@
 package com.blogeditor.service.User;
 
+import com.blogeditor.dto.UserDto;
 import com.blogeditor.models.User;
 import org.springframework.security.core.Authentication;
 
 public interface UserService {
 
-    public User registerUser(User user);
+    public User registerUser(UserDto userDto);
 
     public void checkIfUserExistsByEmail(String email) throws Exception;
 
