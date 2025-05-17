f73b807c7fcc0c437f5798ad7eb7cf9ab716ef51
Amaan63 <amaansayed63@gmail.com>
2025-05-17 19:08:43 +0530

    some styling changes

--- frontend/src/Pages/Auth.jsx
+++ frontend/src/Pages/Auth.jsx
@@ -1,8 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import Login from "../components/Auth/Login";
 import Register from "../components/Auth/Register";
 
-const Auth = ({ setIsAuthenticated }) => {
+const Auth = () => {
+  const location = useLocation();
+
+  // For Button Colo change
+  const isLogin = location.pathname === "/";
+  const isRegister = location.pathname === "/register";
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 to-purple-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-lg">
@@ -14,26 +20,29 @@ const Auth = ({ setIsAuthenticated }) => {
         </div>
 
         <div className="flex justify-center space-x-4">
-          <Link to="/" className="px-4 py-2 rounded-lg bg-blue-600 text-white">
+          <Link
+            to="/"
+            className={`px-4 py-2 rounded-lg ${
+              isLogin ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-700"
+            }`}
+          >
             Login
           </Link>
           <Link
             to="/register"
-            className="px-4 py-2 rounded-lg bg-gray-200 text-gray-700"
+            className={`px-4 py-2 rounded-lg ${
+              isRegister
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200 text-gray-700"
+            }`}
           >
             Register
           </Link>
         </div>
 
         <Routes>
-          <Route
-            path="/"
-            element={<Login setIsAuthenticated={setIsAuthenticated} />}
-          />
-          <Route
-            path="/register"
-            element={<Register setIsAuthenticated={setIsAuthenticated} />}
-          />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </div>
