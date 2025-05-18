26de3b66afdffb4c588efa10b18a6a33f76f00e9
Amaan63 <amaansayed63@gmail.com>
2025-05-18 20:31:21 +0530

    preview the blog we ae creating

--- frontend/src/components/BlogEditor/PreviewContent.jsx
+++ frontend/src/components/BlogEditor/PreviewContent.jsx
@@ -1,22 +1,18 @@
-// components/PreviewContent.jsx
-const PreviewContent = () => {
+const PreviewContent = ({ formValue }) => {
   return (
     <div className="p-6 bg-white rounded-lg shadow">
-      <h1 className="mb-4 text-3xl font-bold">{true || "Untitled Blog"}</h1>
-      <div className="mt-4 prose max-w-none">
-        {/* {content ? (
-          content.split("\n").map((paragraph, index) =>
-            paragraph ? (
-              <p key={index} className="mb-4">
-                {paragraph}
-              </p>
-            ) : (
-              <br key={index} />
-            )
-          )
+      <h1 className="mb-4 text-3xl font-bold">
+        {formValue.title || "Untitled Blog"}
+      </h1>
+      <p className="mb-4 text-gray-600 italic">
+        Tags: {formValue.tags || "No tags"}
+      </p>
+      <div className="mt-4 prose max-w-none whitespace-pre-line">
+        {formValue.content ? (
+          formValue.content
         ) : (
           <p className="text-gray-400">No content yet...</p>
-        )} */}
+        )}
       </div>
     </div>
   );
