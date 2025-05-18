00a806467539527e0adaba2f24dea735afe422eb
Amaan63 <amaansayed63@gmail.com>
2025-05-18 20:30:49 +0530

    added the intial values to pass data from form to preview

--- frontend/src/Pages/BlogEditor.jsx
+++ frontend/src/Pages/BlogEditor.jsx
@@ -4,20 +4,41 @@ import BlogHeader from "../components/BlogEditor/BlogHeader";
 import TabNavigation from "../components/BlogEditor/TabNavigation";
 import BlogForm from "../components/BlogEditor/BlogForm";
 import PreviewContent from "../components/BlogEditor/PreviewContent";
+import { useState } from "react";
+
+const initialValues = {
+  title: "",
+  content: "",
+  tags: "",
+  published: false,
+};
 
 const BlogEditor = () => {
+  const [formValue, setFormValue] = useState(initialValues);
   return (
     <div>
       <BlogHeader />
       <main className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="bg-white rounded-lg shadow p-6">
           <TabNavigation />
+
           <Routes>
-            {/* This route will match when you go to /editor */}
-            <Route path="" element={<BlogForm />} />
-            <Route path="edit" element={<BlogForm />} />
-            <Route path="preview" element={<PreviewContent />} />
-            <Route path="*" element={<Navigate to="/dashboard" />} />
+            <Route
+              path=""
+              element={
+                <BlogForm formValue={formValue} setFormValue={setFormValue} />
+              }
+            />
+            <Route
+              path="edit"
+              element={
+                <BlogForm formValue={formValue} setFormValue={setFormValue} />
+              }
+            />
+            <Route
+              path="preview"
+              element={<PreviewContent formValue={formValue} />}
+            />
           </Routes>
         </div>
       </main>
