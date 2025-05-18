f6c3958adec33e6314d88c089979dd7478b76403
Amaan63 <amaansayed63@gmail.com>
2025-05-18 20:29:06 +0530

    added Get user from token action and logout user action

--- frontend/src/Redux/Authentication/authentication.action.js
+++ frontend/src/Redux/Authentication/authentication.action.js
@@ -1,9 +1,13 @@
 import axios from "axios";
-import { API_BASE_URL } from "../../config/api";
+import { api, API_BASE_URL } from "../../config/api";
 import {
+  GET_USER_FROM_TOKEN_FAILURE,
+  GET_USER_FROM_TOKEN_REQUEST,
+  GET_USER_FROM_TOKEN_SUCCESS,
   LOGIN_FAILURE,
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
+  LOGOUT_USER,
   REGISTER_FAILURE,
   REGISTER_REQUEST,
   REGISTER_SUCCESS,
@@ -52,4 +56,26 @@ export const registerUserAction = (loginData) => async (dispatch) => {
       error?.response?.data?.message || "Registration failed!"; // 🐞 Log any error for debugging
     dispatch({ type: REGISTER_FAILURE, payload: { message: errorMessage } }); // ❌ Dispatch failure action with error
   }
+};
+
+export const getUserFromToken = () => async (dispatch) => {
+  dispatch({ type: GET_USER_FROM_TOKEN_REQUEST });
+  try {
+    const response = await api.get(`${API_BASE_URL}/user/token-to-user`);
+    console.log("Token Retrieved Successfully", response.data);
+
+    // Make sure your payload includes the user object
+    dispatch({ type: GET_USER_FROM_TOKEN_SUCCESS, payload: response.data });
+  } catch (error) {
+    console.log("error ", error);
+    dispatch({ type: GET_USER_FROM_TOKEN_FAILURE, payload: error });
+  }
+};
+
+export const logoutUser = () => (dispatch) => {
+  // Remove token from localStorage or wherever you keep it
+  localStorage.removeItem("jwt");
+
+  // Dispatch logout action to clear redux state
+  dispatch({ type: LOGOUT_USER });
 };
