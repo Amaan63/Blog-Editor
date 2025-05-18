3af6fcd95de26a8fbf8dc4b96e29be0eb10d68ab
Amaan63 <amaansayed63@gmail.com>
2025-05-18 17:57:06 +0530

    imported blogReducer

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -3,10 +3,12 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 // redux-thunk middleware import kar rahe hain, async actions ke liye
 import { thunk } from "redux-thunk"; // ✅ named import
 import { authenticationReducer } from "./Authentication/authentication.reducer";
+import blogReducer from "./Blog/blog.reducer";
 
 // combineReducers se multiple reducers ko ek sath combine karne ka structure banaya gaya hai
 const rootReducers = combineReducers({
   auth: authenticationReducer,
+  blog: blogReducer,
 });
 
 // legacy_createStore se store banaya ja raha hai
