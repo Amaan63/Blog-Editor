d6a13a8e9ae0967021ba0b6bd0a85a808b6985d5
Amaan63 <amaansayed63@gmail.com>
2025-05-18 21:32:39 +0530

    added keyDown event

--- frontend/src/components/Dashboard/SearchBar.jsx
+++ frontend/src/components/Dashboard/SearchBar.jsx
@@ -1,12 +1,18 @@
 import { Search } from "lucide-react";
 
 const SearchBar = ({ value, onChange, onSearch }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onSearch();
+    }
+  };
   return (
     <div className="flex items-center space-x-2">
       <input
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
+        onKeyDown={handleKeyDown}
         placeholder="Search blog by ID"
         className="flex-grow px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
