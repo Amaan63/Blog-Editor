7ebf37e1918d1c25df4bcb3d8598a46980edc77f
Amaan63 <amaansayed63@gmail.com>
2025-05-18 17:56:41 +0530

    created blog action

---
+++ frontend/src/Redux/Blog/blog.action.js
@@ -0,0 +1,36 @@
+import { api, API_BASE_URL } from "../../config/api";
+import {
+  PUBLISH_BLOG_FAILURE,
+  PUBLISH_BLOG_REQUEST,
+  PUBLISH_BLOG_SUCCESS,
+  SAVE_OR_UPDATE_DRAFT_BLOG_FAILURE,
+  SAVE_OR_UPDATE_DRAFT_BLOG_REQUEST,
+  SAVE_OR_UPDATE_DRAFT_BLOG_SUCCESS,
+} from "./blog.actionType";
+
+export const createOrUpdateBlogAction = (blogData) => async (dispatch) => {
+  dispatch({ type: SAVE_OR_UPDATE_DRAFT_BLOG_REQUEST });
+  try {
+    const { data } = await api.post(
+      `${API_BASE_URL}/blogs/save-draft`,
+      blogData
+    );
+    dispatch({ type: SAVE_OR_UPDATE_DRAFT_BLOG_SUCCESS, payload: data });
+    console.log("Draft Blog ", data);
+  } catch (error) {
+    console.log("error ", error);
+    dispatch({ type: SAVE_OR_UPDATE_DRAFT_BLOG_FAILURE, payload: error });
+  }
+};
+
+export const publishBlogAction = (blogData) => async (dispatch) => {
+  dispatch({ type: PUBLISH_BLOG_REQUEST });
+  try {
+    const { data } = await api.post(`${API_BASE_URL}/blogs/publish`, blogData);
+    dispatch({ type: PUBLISH_BLOG_SUCCESS, payload: data });
+    console.log("Published Blog ", data);
+  } catch (error) {
+    console.log("error ", error);
+    dispatch({ type: PUBLISH_BLOG_FAILURE, payload: error });
+  }
+};
