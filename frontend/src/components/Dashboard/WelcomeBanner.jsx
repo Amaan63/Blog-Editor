c9879db55905e111992a89957e414a703b672227
Amaan63 <amaansayed63@gmail.com>
2025-05-18 21:32:46 +0530

    some changes

--- frontend/src/components/Dashboard/WelcomeBanner.jsx
+++ frontend/src/components/Dashboard/WelcomeBanner.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const WelcomeBanner = ({ user }) => {
   return (
-    <div className="text-2xl text-center p-3 m-3">
-      Hello {user?.username || "Username Not Found"}
+    <div className="text-2xl text-center p-3 m-3 font-semibold">
+      Hello, {user?.username || "Loading"}
     </div>
   );
 };
