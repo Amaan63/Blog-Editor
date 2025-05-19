9d8880643be0d7c4d1caeec80fcb6ebb48632eb9
Amaan63 <amaansayed63@gmail.com>
2025-05-19 14:23:37 +0530

    deleteBlogByIdAction is created

--- frontend/src/Redux/Blog/blog.action.js
+++ frontend/src/Redux/Blog/blog.action.js
@@ -1,5 +1,8 @@
 import { api, API_BASE_URL } from "../../config/api";
 import {
+  DELETE_BLOG_BY_ID_FAILURE,
+  DELETE_BLOG_BY_ID_REQUEST,
+  DELETE_BLOG_BY_ID_SUCCESS,
   GET_ALL_BLOG_FAILURE,
   GET_ALL_BLOG_REQUEST,
   GET_ALL_BLOG_SUCCESS,
@@ -63,11 +66,28 @@ export const getBlogByIdAction = (id) => async (dispatch) => {
   try {
     const { data } = await api.get(`${API_BASE_URL}/blogs/${id}`);
     dispatch({ type: GET_BLOG_BY_ID_SUCCESS, payload: data });
-    console.log("GET Post By ID ---  ", data);
+    console.log("GET Blog By ID ---  ", data);
   } catch (error) {
     const errorMessage = error?.response?.data?.message || "Blog Not found"; // 🐞 Log any error for debugging
     dispatch({
       type: GET_ALL_BLOG_FAILURE,
+      payload: { message: errorMessage },
+    }); // ❌ Dispatch failure action with error
+  }
+};
+
+export const deleteBlogByIdAction = (id) => async (dispatch) => {
+  dispatch({ type: DELETE_BLOG_BY_ID_REQUEST });
+  try {
+    const { data } = await api.delete(
+      `${API_BASE_URL}/blogs/delete/blogId/${id}`
+    );
+    dispatch({ type: DELETE_BLOG_BY_ID_SUCCESS, payload: id });
+    console.log("Delete Blog By ID ---  ", data);
+  } catch (error) {
+    const errorMessage = error?.response?.data?.message || "Blog Not found"; // 🐞 Log any error for debugging
+    dispatch({
+      type: DELETE_BLOG_BY_ID_FAILURE,
       payload: { message: errorMessage },
     }); // ❌ Dispatch failure action with error
   }
