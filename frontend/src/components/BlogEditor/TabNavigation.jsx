1b4234e37d92d13104b135b7958c5046907775c9
Amaan63 <amaansayed63@gmail.com>
2025-05-19 11:10:35 +0530

    simplified teh Tab navigation with the header

--- frontend/src/components/BlogEditor/TabNavigation.jsx
+++ frontend/src/components/BlogEditor/TabNavigation.jsx
@@ -11,29 +11,8 @@ const TabNavigation = () => {
   const isPreview = location.pathname.startsWith("/editor/preview");
 
   return (
-    <div className="flex border-b border-gray-200 mb-4">
-      <Link
-        to="/editor"
-        className={`px-4 py-2 text-sm font-medium ${
-          isEdit
-            ? "text-blue-600 border-b-2 border-blue-600"
-            : "text-gray-500 hover:text-gray-700"
-        }`}
-      >
-        <PenLine size={16} className="inline mr-1" />
-        Edit
-      </Link>
-      <Link
-        to="/editor/preview"
-        className={`px-4 py-2 ml-4 text-sm font-medium ${
-          isPreview
-            ? "text-blue-600 border-b-2 border-blue-600"
-            : "text-gray-500 hover:text-gray-700"
-        }`}
-      >
-        <Eye size={16} className="inline mr-1" />
-        Preview
-      </Link>
+    <div className="flex border-b border-gray-200 mb-4  justify-center text-xl m-3 font-semibold">
+      <h1>Blog Form</h1>
     </div>
   );
 };
